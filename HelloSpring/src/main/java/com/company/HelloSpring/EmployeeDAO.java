92fe03e40308b89c977c816d3777676f3cdb17f9
suhyunkim <s_u_o_o@naver.com>
2020-01-20 16:07:15 +0900

    Spring#2 Hello Spring Project

---
+++ HelloSpring/src/main/java/com/company/HelloSpring/EmployeeDAO.java
@@ -0,0 +1,9 @@
+package com.company.HelloSpring;
+
+import java.util.List;
+import com.company.HelloSpring.EmployeeVO;
+
+public interface EmployeeDAO {
+	public List<EmployeeVO> getAllEmployees();
+	
+}
---
+++ HelloSpring/src/main/java/com/company/HelloSpring/EmployeeManager.java
@@ -0,0 +1,10 @@
+package com.company.HelloSpring;
+
+import java.util.List;
+
+import com.company.HelloSpring.EmployeeVO;
+ 
+public interface EmployeeManager 
+{
+    public List<EmployeeVO> getAllEmployees();
+}
